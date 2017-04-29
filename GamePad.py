4f6519bd60692fb49ce532ea89a78c1019f091be
mkatzef <marckatzef@gmail.com>
2017-04-29 13:48:33 +1200

    Added existing STACKER controller script.

---
+++ GamePad.py
@@ -0,0 +1,41 @@
+import serial
+from tkinter import *
+
+class GamePad:
+	def __init__(self, window, ser):
+		self.ser = ser
+		trigger_button = Button(window, text="NOW", command=self.trigger, height=5, width=20)
+		trigger_button.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+
+		window.bind('<space>', self.trigger)
+		window.bind('<Return>', self.trigger)
+		
+		for letter in [chr(c) for c in range(ord('a'), ord('z') + 1)]:
+			window.bind('<' + letter + '>', self.trigger)
+		
+		for number in range(10):
+			window.bind('<' + str(number) + '>', self.trigger)
+				
+
+	def trigger(self, *args):
+		self.ser.write(b' ')
+
+
+def main(ser):
+	window = Tk()
+	window.title("Game Pad")
+	window.minsize(300, 300)
+	GamePad(window, ser)
+	window.mainloop()
+
+
+if __name__ == "__main__":
+	try:
+		port_name = 'COM4'
+		ser = serial.Serial(port_name, 9600)
+	except:
+		port_name = input("Port name:")
+		ser = serial.Serial(port_name, 9600)
+		
+	main(ser)
+	ser.close()
